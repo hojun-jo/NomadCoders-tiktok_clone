9b29006d69ce33e02687e57092d3957e006031ec
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-11-12 10:52:08 +0900

    feat: Video Timeline - RefreshIndicator 추가

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -38,6 +38,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
           Offstage(
