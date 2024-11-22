faaced8bfde3c27699b760f65820fee89d6659da
조호준 <hojun@johojun-ui-MacBookAir.local>
2024-11-22 16:50:46 +0900

    refactor: l10n - SignUpScreen에 익스텐션으로 자동 생성된 코드 적용
    flutter intl

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -5,7 +5,7 @@ import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/username_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:flutter_gen/gen_l10n/intl_generated.dart';
+import 'package:tiktok_clone/generated/l10n.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -38,7 +38,7 @@ class SignUpScreen extends StatelessWidget {
                 children: [
                   Gaps.v80,
                   Text(
-                    AppLocalizations.of(context)!.signUpTitle("TikTok"),
+                    S.of(context).signUpTitle("TikTok"),
                     style: const TextStyle(
                       fontSize: Sizes.size24,
                       fontWeight: FontWeight.w700,
@@ -48,7 +48,7 @@ class SignUpScreen extends StatelessWidget {
                   Opacity(
                     opacity: 0.7,
                     child: Text(
-                      "Create a profile, follow other accounts, make your own videos, and more.",
+                      S.of(context).signUpSubtitle,
                       style: Theme.of(context).textTheme.titleMedium,
                       textAlign: TextAlign.center,
                     ),
@@ -58,13 +58,13 @@ class SignUpScreen extends StatelessWidget {
                     AuthButton(
                       onTap: () => _onEmailTap(context),
                       icon: const FaIcon(FontAwesomeIcons.user),
-                      text: "Use email & password",
+                      text: S.of(context).emailPasswordButton,
                     ),
                     Gaps.v16,
                     AuthButton(
                       onTap: () => {},
                       icon: const FaIcon(FontAwesomeIcons.apple),
-                      text: "Continue width Apple",
+                      text: S.of(context).appleButton,
                     ),
                   ],
                   if (orientation == Orientation.landscape)
@@ -74,7 +74,7 @@ class SignUpScreen extends StatelessWidget {
                           child: AuthButton(
                             onTap: () => _onEmailTap(context),
                             icon: const FaIcon(FontAwesomeIcons.user),
-                            text: "Use email & password",
+                            text: S.of(context).emailPasswordButton,
                           ),
                         ),
                         Gaps.h16,
@@ -82,7 +82,7 @@ class SignUpScreen extends StatelessWidget {
                           child: AuthButton(
                             onTap: () => {},
                             icon: const FaIcon(FontAwesomeIcons.apple),
-                            text: "Continue width Apple",
+                            text: S.of(context).appleButton,
                           ),
                         ),
                       ],
@@ -96,12 +96,17 @@ class SignUpScreen extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("Already have an account?"),
+                Text(
+                  S.of(context).alreadyHaveAnAccount,
+                  style: const TextStyle(
+                    fontSize: Sizes.size16,
+                  ),
+                ),
                 Gaps.h5,
                 GestureDetector(
                   onTap: () => _onLoginTap(context),
                   child: Text(
-                    "Log in",
+                    S.of(context).logIn,
                     style: TextStyle(
                       fontWeight: FontWeight.w600,
                       color: Theme.of(context).primaryColor,
