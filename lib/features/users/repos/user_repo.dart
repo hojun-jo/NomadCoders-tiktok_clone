64ee1ce2e402dc477b9fa60bf67dd84a2f1ceabe
조호준 <hojun@johojun-ui-MacBookAir.local>
2025-01-29 09:59:11 +0900

    feat: 유저 프로필 확인을 위한 model, repo, viewmodel 추가

---
+++ lib/features/users/repos/user_repo.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/users/models/user_profile_model.dart';
+
+class UserRepository {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> createProfile(UserProfileModel user) async {}
+
+  // create profile
+  // get profile
+  // TODO: update
+  // avatar
+  // bio
+  // link
+}
+
+final userRepo = Provider(
+  (ref) => UserRepository(),
+);
